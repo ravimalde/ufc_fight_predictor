d21b4122ba97efea972d662224fd2d9351a83ca3
ravi malde <ravidmalde@gmail.com>
2020-03-20 22:34:39 +0000

    making fight spider

---
+++ ufc_scrape/ufc_scrape/spiders/fights.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class FightsSpider(scrapy.Spider):
+    name = 'fights'
+    allowed_domains = ['www.ufcstats.com']
+    start_urls = ['http://www.ufcstats.com/statistics/events/completed']
+
+    def parse(self, response):
+        events = response.xpath("//tr/td/i/a")
+        for event in events:
+            event_name = event.xpath(".//text()").get()
+            self.event_name = event_name
+            link = event.xpath(".//@href").get()
+        
+            yield response.follow(url = link, callback=self.parse_event, meta={'event_name': event_name})
+
+    def parse_event(self, response):
+        rows = response.xpath("(//table[@class='b-fight-details__table b-fight-details__table_style_margin-top b-fight-details__table_type_event-details js-fight-table'])/tbody/tr")
+        for row in rows:
+            f1_name = row.xpath(".//td[2]/p[1]/a/text()").get()
+            f2_name = row.xpath(".//td[2]/p[2]/a/text()").get()
+            f1_str = int(row.xpath(".//td[3]/p[1]/text()").get())
+            f2_str = int(row.xpath(".//td[3]/p[2]/text()").get())
+            f1_tds = int(row.xpath(".//td[4]/p[1]/text()").get())
+            f2_tds = int(row.xpath(".//td[4]/p[2]/text()").get())
+            f1_subs = int(row.xpath(".//td[5]/p[1]/text()").get())
+            f2_subs = int(row.xpath(".//td[5]/p[2]/text()").get())
+            f1_pass = int(row.xpath(".//td[6]/p[1]/text()").get())
+            f2_pass = int(row.xpath(".//td[6]/p[2]/text()").get())
+            weight_class = row.xpath(".//td[7]/p/text()").get()
+            method = row.xpath(".//td[8]/p[1]/text()").get()
+            finisher = row.xpath(".//td[8]/p[2]/text()").get()
+            round_num = row.xpath(".//td[9]/p/text()").get()
+            time = row.xpath(".//td[10]/p/text()").get()
+            yield{
+                'f1_name': f1_name,
+                'f2_name': f2_name,
+                'f1_str': f1_str,
+                'f2_str': f2_str,
+                'f1_tds': f1_tds,
+                'f2_tds': f2_tds,
+                'f1_subs': f1_subs,
+                'f2_subs': f2_subs,
+                'f1_pass': f1_pass,
+                'f2_pass': f2_pass,
+                'weight_class': weight_class,
+                'method': method,
+                'finisher': finisher,
+                'round_num': round_num,
+                'time': time,
+            }
