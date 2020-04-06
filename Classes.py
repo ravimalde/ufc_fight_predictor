e23849369292fab8fd8a391c8a4ddd0c5062a204
ravi malde <ravidmalde@gmail.com>
2020-04-06 11:14:45 +0100

    testing classification models

---
+++ Classes.py
@@ -0,0 +1,339 @@
+
+import pandas as pd
+from datetime import date, timedelta
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import itertools
+
+from IPython.display import display
+from IPython.display import Image
+import pydotplus
+from sklearn.externals.six import StringIO 
+from subprocess import call
+
+import warnings
+import tensorflow as tf
+from tensorflow import keras
+from sklearn.model_selection import train_test_split, GridSearchCV, StratifiedKFold
+from sklearn.preprocessing import StandardScaler
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Convolution2D, MaxPooling2D
+from keras.optimizers import Adam
+from keras.regularizers import l2
+
+from sklearn.linear_model import LogisticRegression, LinearRegression
+from sklearn.ensemble import VotingClassifier, BaggingClassifier, AdaBoostClassifier, RandomForestClassifier
+from sklearn.compose import ColumnTransformer
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from deslib.static.stacked import StackedClassifier
+from sklearn.metrics import confusion_matrix, roc_curve, roc_auc_score, classification_report
+from sklearn.ensemble import StackingClassifier
+from xgboost.sklearn import XGBClassifier
+from sklearn import tree
+from sklearn.tree import export_graphviz
+import pickle
+
+pd.set_option('display.max_columns', 500)
+warnings.filterwarnings("ignore")
+
+
+
+class Classification():
+    
+    def __init__(self, model, X_train, X_val, y_train, y_val):
+        self.model = model
+        self.X_train = X_train
+        self.y_train = y_train
+        self.X_val = X_val
+        self.y_val = y_val
+        self.scores_table = pd.DataFrame()
+        
+        if self.model == 'Logistic Regression':
+            self.instantiate = LogisticRegression(fit_intercept=False)
+        elif self.model == 'Decision Tree':
+            self.instantiate = DecisionTreeClassifier(random_state=50)
+        elif self.model == 'Random Forest':
+            self.instantiate = RandomForestClassifier(n_estimators=500, 
+                                                          n_jobs=-1,
+                                                          random_state=50)
+        elif self.model == "SVM":
+            self.instantiate = SVC(probability=True,
+                                   random_state=50)
+        
+# SCORE FUNCTION -------------------------------------------------------------
+        
+    def scores(self, model, X_train, X_val, y_train, y_val):
+        
+        train_prob = model.predict_proba(X_train)[:,1]
+        val_prob = model.predict_proba(X_val)[:,1]
+        
+        train_auc = roc_auc_score(y_train, train_prob)
+        val_auc = roc_auc_score(y_val, val_prob)
+        
+        self.train_auc = train_auc
+        self.val_auc = val_auc
+        
+        self.scores_table = pd.DataFrame()
+        
+        self.scores_table["Model"] = [self.model]
+        self.scores_table["Train AUC"] = [self.train_auc]
+        self.scores_table["Validation AUC"] = [self.val_auc]
+        
+        return self.scores_table
+    
+# THRESHOLD ANNOTATION -------------------------------------------------------
+
+    def annot(fpr,tpr,thr):
+        k=0
+        for i,j in zip(fpr,tpr):
+            if k % 100 == 0:
+                plt.annotate(round(thr[k],2),xy=(i,j), textcoords='data')
+            k+=1
+            
+# ROC PLOT FUNCTION ----------------------------------------------------------
+
+    def roc_plot(model, X_train, X_val, y_train, y_val):
+        train_prob = model.predict_proba(X_train)[:,1]
+        val_prob = model.predict_proba(X_val)[:,1]
+        plt.figure(figsize=(7,7))
+        for data in [[y_train, train_prob],[y_val, val_prob]]:
+            fpr, tpr, thresh = roc_curve(data[0], data[1])
+            plt.plot(fpr, tpr)
+        Classification.annot(fpr, tpr, thresh)
+        plt.plot([0, 1], [0, 1], color='navy', linestyle='--')
+        plt.ylabel('TPR (power)')
+        plt.xlabel('FPR (alpha)')
+        plt.legend(['train','val'])
+        plt.show()
+        
+# GET SCORES FUNCTION --------------------------------------------------------
+
+    def get_scores(self, param_grid, cv_type):
+        reg = self.instantiate
+        fit_reg = reg.fit(self.X_train, self.y_train)
+        opt_model = GridSearchCV(fit_reg,
+                                 param_grid,
+                                 cv=cv_type,
+                                 scoring='roc_auc',
+                                 return_train_score=True,
+                                 n_jobs=-1)
+        self.opt_model = opt_model.fit(self.X_train, self.y_train) 
+        self.best_model = opt_model.best_estimator_
+        self.scores = Classification.scores(self,
+                                            self.best_model,
+                                            self.X_train,
+                                            self.X_val,
+                                            self.y_train,
+                                            self.y_val)
+        self.best_params = opt_model.best_params_
+        display(self.scores_table)
+        print("The best hyperparameters are: ", self.best_params,'\n')
+        self.roc_plot = Classification.roc_plot(self.best_model,
+                                                self.X_train,
+                                                self.X_val,
+                                                self.y_train,
+                                                self.y_val)
+        self.y_predicted = opt_model.predict(self.X_val)
+        
+# OPTIMUM PLOT FUCNTION ------------------------------------------------------
+
+    def opt_plots(self):
+        
+        if self.model == "Decision Tree" or self.model == "Random Forest":
+            opt = pd.DataFrame(self.opt_model.cv_results_)
+            cols = [col for col in opt.columns if ('mean' in col or 'std' in col) and 'time' not in col]
+            params = pd.DataFrame(list(opt.params))
+            opt = pd.concat([params, opt[cols]], axis=1, sort=False)
+            
+            plt.figure(figsize=[15,4])
+            plt.subplot(121)
+            sns.heatmap(pd.pivot_table(opt,
+                                       index='max_depth',
+                                       columns='min_samples_leaf',
+                                       values='mean_train_score')*100)
+            plt.title('ROC_AUC - Training')
+            plt.subplot(122)
+            sns.heatmap(pd.pivot_table(opt,
+                                       index='max_depth',
+                                       columns='min_samples_leaf',
+                                       values='mean_test_score')*100)
+            plt.title('ROC_AUC - Validation')
+            
+        else:
+            print("This model does not have an optimum hyperparameter plot!")
+        
+# CONFUSION MATRIX FUNCTION --------------------------------------------------
+
+    def conf_matrix(y_true, y_pred):
+        
+        cm = {'TP': 0, 'TN': 0, 'FP': 0, 'FN': 0}
+        
+        for i, label in enumerate(y_true):
+            
+            pred = y_pred[i]
+            if label == 1:
+                if label == pred:
+                    cm['TP'] += 1
+                else:
+                    cm['FN'] += 1
+            else:
+                if label == pred:
+                    cm['TN'] += 1
+                else:
+                    cm['FP'] += 1
+                
+        return cm
+        
+# DISPLAY PRETTY CONFUSION MATRIX FUNCTION -----------------------------------
+
+    def pretty_conf_matrix(self):
+        
+        Classification.conf_matrix(self.y_val, self.y_predicted)
+        cnf_matrix = confusion_matrix(self.y_val, self.y_predicted)
+        
+        plt.figure(figsize=(6,6))
+        plt.imshow(cnf_matrix, cmap=plt.cm.Greens)
+        
+        plt.title('Confusion Matrix')
+        plt.ylabel('True label')
+        plt.xlabel('Predicted label')
+        
+        class_names = set(y)
+        tick_marks = np.arange(1,2,1)
+        plt.xticks(tick_marks, [1,0], rotation=45)
+        plt.yticks(tick_marks, [1,0])
+
+        thresh = cnf_matrix.max() / 2.
+        for i, j in itertools.product(range(cnf_matrix.shape[0]), range(cnf_matrix.shape[1])):
+                plt.text(j, i, cnf_matrix[i, j],
+                         horizontalalignment='center')
+        plt.colorbar
+
+# PLOT DECISION TREE FUCNTION ------------------------------------------------
+
+    def plot_dtree(self):
+        
+        if self.model == "Decision Tree" or self.model == "Random Forest":
+            
+            dot_data = StringIO()
+            export_graphviz(self.best_model, 
+                            out_file=dot_data, 
+                            filled=True,
+                            rounded=True,
+                            special_characters=True)
+            graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
+            graph.write_png(f'{self.model}.png')
+            image = mpimg.imread(f'{self.model}.png')
+            plt.figure(figsize=(50,50))
+            plt.imshow(image)
+            plt.show()
+            
+        else:
+            
+            print("This model does not have a decision tree plot!")
+
+class Ensemble(Classification):
+    
+    def __init__(self, ensemble_method, estimators, X_train, X_val, y_train, y_val):
+        
+        self.ensemble_method = ensemble_method
+        self.X_train = X_train
+        self.y_train = y_train
+        self.X_val = X_val
+        self.y_val = y_val
+        
+        if self.ensemble_method == "Voting":
+            self.instantiate = VotingClassifier(estimators=estimators, voting='soft', n_jobs=-1)
+        elif self.ensemble_method == "AdaBoost":
+            self.instantiate = AdaBoostClassifier(estimators, algorithm='SAMME', n_estimators=100)
+        elif self.ensemble_method == "XGBoost":
+            self.instantiate = XGBClassifier(n_jobs=-1)
+        elif self.ensemble_method == "Stacking":
+            self.instantiate = StackingClassifier(estimators)
+            
+    def ensemble_scores(self):
+
+        train_prob = self.instantiate.predict_proba(self.X_train)[:,1]
+        val_prob = self.instantiate.predict_proba(self.X_val)[:,1]
+
+        train_auc = roc_auc_score(self.y_train, train_prob)
+        val_auc = roc_auc_score(self.y_val, val_prob)
+
+        self.train_auc = train_auc
+        self.val_auc = val_auc
+
+        self.scores_table = pd.DataFrame()
+
+        self.scores_table["Model"] = [self.ensemble_method]
+        self.scores_table["Train AUC"] = [self.train_auc]
+        self.scores_table["Validation AUC"] = [self.val_auc]
+
+        return self.scores_table
+    
+    def ensemble_get_scores(self, param_grid, cv_type):
+        reg = self.instantiate
+        fit_reg = reg.fit(self.X_train, self.y_train)
+        opt_model = GridSearchCV(fit_reg,
+                                 param_grid,
+                                 cv=cv_type,
+                                 scoring='roc_auc',
+                                 return_train_score=True,
+                                 n_jobs=-1)
+        self.opt_model = opt_model.fit(self.X_train, self.y_train) 
+        self.best_model = opt_model.best_estimator_
+        self.scores = Ensemble.ensemble_scores(self)
+        self.best_params = opt_model.best_params_
+        display(self.scores_table)
+        self.roc_plot = Classification.roc_plot(self.best_model,
+                                                self.X_train,
+                                                self.X_val,
+                                                self.y_train,
+                                                self.y_val)
+        self.y_predicted = opt_model.predict(self.X_val)
+        
+    def conf_matrix(y_true, y_pred):
+        
+        cm = {'TP': 0, 'TN': 0, 'FP': 0, 'FN': 0}
+        
+        for i, label in enumerate(y_true):
+            
+            pred = y_pred[i]
+            if label == 1:
+                if label == pred:
+                    cm['TP'] += 1
+                else:
+                    cm['FN'] += 1
+            else:
+                if label == pred:
+                    cm['TN'] += 1
+                else:
+                    cm['FP'] += 1
+                
+        return cm
+    
+    def ensemble_pretty_conf_matrix(self):
+        
+        Ensemble.conf_matrix(self.y_val, self.y_predicted)
+        cnf_matrix = confusion_matrix(self.y_val, self.y_predicted)
+        
+        plt.figure(figsize=(6,6))
+        plt.imshow(cnf_matrix, cmap=plt.cm.Greens)
+        
+        plt.title('Confusion Matrix')
+        plt.ylabel('True label')
+        plt.xlabel('Predicted label')
+        
+        class_names = set(y)
+        tick_marks = np.arange(1,2,1)
+        plt.xticks(tick_marks, [1,0], rotation=45)
+        plt.yticks(tick_marks, [1,0])
+
+        thresh = cnf_matrix.max() / 2.
+        for i, j in itertools.product(range(cnf_matrix.shape[0]), range(cnf_matrix.shape[1])):
+                plt.text(j, i, cnf_matrix[i, j],
+                         horizontalalignment='center')
+        plt.colorbar
